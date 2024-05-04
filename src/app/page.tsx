19db01f4fc4845b856d73081897eaab8c53a1b4a
Abhijith Babu <abijithb2002@gmail.com>
2024-05-04 17:17:21 +0530

    changed the bowl image error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,34 @@
 "use client";
 import * as React from "react";
-import HomePage from "@/components/Home/Home";
-import Footer from "@/components/footer";
-import {NextUIProvider} from "@nextui-org/react";
-import Image from "next/image";
-import bg from '../../public/image/foodbowllast.png'
-import bgMb from '../../public/image/foodbowlmoblie.png'
-import ParallaxText from "@/components/Home/parallax/ParallaxText";
+import HomePage from '@/components/Home/Home';
+import Footer from '@/components/footer';
+import { NextUIProvider } from '@nextui-org/react';
+import Image from 'next/image';
+import bg from '../../public/image/foodbowllast.png';
+import bgMb from '../../public/image/foodbowlmoblie.png';
+import ParallaxText from '@/components/Home/parallax/ParallaxText';
 
 export default function Home() {
   const [section, setSection] = React.useState(0)
-
-  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
-
-  // Update window width when component mounts
-  React.useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    // Add event listener to update window width
-    window.addEventListener('resize', handleResize);
-
-    // Remove event listener on component unmount
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-  ;
- 
-  const imageSrc = windowWidth <= 640 ? bgMb : bg;
-  // const classes = isMediumScreen ? '-z-5 h-100 object-bottom object-contain' : '-z-5 object-bottom object-contain';
   return (
     <NextUIProvider>
-    <main className="flex min-h-screen flex-col items-center justify-between relative w-full h-full">
-    {section === 0 && (
+      <main className="flex min-h-screen flex-col items-center justify-between relative w-full h-full">
+      {section === 0 && (
           // <Image src={bg} alt="background-image" fill className="-z-5 h-100 object-bottom object-contain sm:object-none sm:h-46 sm:w-150"/>
-          <Image src={imageSrc} alt="background-image" fill className="-z-5 object-bottom object-contain "/>
+          <>
+          <Image src={bg} alt="background-image" fill className="-z-5 object-bottom object-contain hidden sm:block"/>
+          <Image src={bgMb} alt="background-image" fill className="-z-5 object-bottom object-contain sm:hidden" />
+          </>
 
         )} 
-      {/* <ParallaxText  baseVelocity={1}>INCREDIBLE FOOD CHOICES WITH AWESOME REWARD POINTS</ParallaxText> */}
-      {/* object-cover object-center */}
-      <div className="w-full h-full">
-
-      <HomePage section={section} setSection={setSection}/>
-      </div>
-    </main>
+        <div className="w-full h-full">
+          <HomePage section={section} setSection={setSection} /> 
+        </div>
+        {/* Background image */}
+        {/* <div className="w-full h-full absolute top-0 left-0">
+          <Image src={bg} alt="background-image" fill className="object-bottom object-contain sm:hidden" />
+        </div> */}
+      </main>
     </NextUIProvider>
   );
 }
