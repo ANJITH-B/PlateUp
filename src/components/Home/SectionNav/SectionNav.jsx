f4df206b5858416a0c2e24887fe47245a3a01b59
Anjith B <anjithb@Anjiths-MacBook-Air.local>
2024-06-13 13:25:43 +0530

    fixed alginment on secction navbar

--- src/components/Home/SectionNav/SectionNav.jsx
+++ src/components/Home/SectionNav/SectionNav.jsx
@@ -3,11 +3,11 @@ export const SectionNav = (props) => {
     return (
         <>
             <div className={`z-10 fixed top-0 right-0  bottom-0 bg-gradient-to-b from-[#213] to-[#213] transition-all overflow-hidden flex flex-col items-center w-full ${[0, 5, 6, 7, 8,9].includes(section) ? 'h-0' : 'h-20'}`}>
-                <div className="flex flex-row items-center justify-evenly sm:gap-10 gap-14 p-7 ">
-                    <div className=" w-[10vw]"><button onClick={() => onSectionChange(1)} className={` md:text-2xl font-secondary  font-bold  ${section === 1 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>Food</button ></div>
-                    <div className=" w-[10vw]"><button onClick={() => onSectionChange(2)} className={` md:text-2xl font-secondary  font-bold  ${section === 2 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>DineIn</button ></div>
-                    <div className=" w-[10vw]"><button onClick={() => onSectionChange(3)} className={` md:text-2xl font-secondary  font-bold  ${section === 3 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>Grocery</button ></div>
-                    <div className=" w-[10vw]"><button onClick={() => onSectionChange(4)} className={` md:text-2xl font-secondary  font-bold  ${section === 4 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>Travel</button ></div>
+                <div className="flex flex-row items-center justify-between gap-4 md:gap-14 pt-7 md:p-7 ">
+                    <div className=" md:w-[10vw]"><button onClick={() => onSectionChange(1)} className={` md:text-2xl font-secondary  font-bold  ${section === 1 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>Food</button ></div>
+                    <div className=" md:w-[10vw]"><button onClick={() => onSectionChange(2)} className={` md:text-2xl font-secondary  font-bold  ${section === 2 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>DineIn</button ></div>
+                    <div className=" md:w-[10vw]"><button onClick={() => onSectionChange(3)} className={` md:text-2xl font-secondary  font-bold  ${section === 3 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>Grocery</button ></div>
+                    <div className=" md:w-[10vw]"><button onClick={() => onSectionChange(4)} className={` md:text-2xl font-secondary  font-bold  ${section === 4 ? 'bg-clip-text text-transparent bg-gradient-to-b from-[#FFC700] to-[#FF5107]' : 'text-white'}`}>Travel</button ></div>
                 </div>
             </div>
         </>
