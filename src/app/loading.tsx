e4ed7c26aa777bac6911f30917187f309c15600e
Anjith B <anjithb@Anjiths-MacBook-Air.local>
2024-06-27 15:38:47 +0530

    preloader changed new preloader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,102 @@
+'use client'
 import React from "react";
+import { animate, motion, useMotionValue, useTransform } from "framer-motion";
+
+
+const normal = {
+  animate: {
+    x: [0,-1035],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: 5,
+        ease: "linear",
+      },
+    },
+  },
+};
+const slow = {
+  animate: {
+    x: [0,-705],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: 5,
+        ease: "linear",
+      },
+    },
+  },
+};
 
 const loading = () => {
+  const count = useMotionValue(0);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    const number = useTransform(count, Math.round);
+
+    React.useEffect(() => {
+        const animation = animate(count, 100, { duration: 0.5 });
+    }, []);
+
   return (
-    <div className="flex items-center justify-center w-full h-[100vh] bg-black">
-      <span className="loader"></span>
+    // <div className="flex items-center justify-center w-full h-[100vh] bg-black">
+    //   <span className="loader">hi</span>
+    // </div>
+    <>
+
+    <motion.div className=" absolute z-10 items-center my-[39vh] mx-[39vw]">
+    <div className="flex flex-row">
+  <motion.h1 className=' text-black dark:text-white font-bold text-6xl '>{number}</motion.h1>
+    <h1 className="text-6xl font-bold">%</h1>
     </div>
+  <motion.div className="bg-gray-500 w-80 h-20" >
+  <motion.div
+  className="bg-white h-20"
+  style={{ width: '1rem' }} // Initial width
+  animate={{ width: '20rem' }} // Target width
+  transition={{ duration: 0.5 }} // Duration of the animation
+  >
+</motion.div>
+  </motion.div>
+  </motion.div>
+
+   <div className="marquee">
+    <motion.div className="track" variants={normal} animate="animate" >
+      <h1>
+        Let's Work Together. Let's Work Together. Let's Work Together. Let's
+        Work Together. Let's Work Together. Let's Work Together. Let's Work
+        Together
+      </h1>
+    </motion.div>
+  </div>
+   <div className="marquee">
+    <motion.div className="track" variants={slow} animate="animate">
+      <h1>
+        Let's Work Together. Let's Work Together. Let's Work Together. Let's
+        Work Together. Let's Work Together. Let's Work Together. Let's Work
+        Together
+      </h1>
+    </motion.div>
+  </div>
+   <div className="marquee">
+    <motion.div className="track" variants={normal} animate="animate">
+      <h1>
+        Let's Work Together. Let's Work Together. Let's Work Together. Let's
+        Work Together. Let's Work Together. Let's Work Together. Let's Work
+        Together
+      </h1>
+    </motion.div>
+  </div>
+   <div className="marquee">
+    <motion.div className="track" variants={slow} animate="animate">
+      <h1>
+        Let's Work Together. Let's Work Together. Let's Work Together. Let's
+        Work Together. Let's Work Together. Let's Work Together. Let's Work
+        Together
+      </h1>
+    </motion.div>
+  </div>
+  </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,19 @@
 "use client";
 import * as React from "react";
 import HomePage from '@/components/Home/Home';
-import Footer from '@/components/footer';
 import { NextUIProvider } from '@nextui-org/react';
-import Image from 'next/image';
-import bg from '../../public/image/foodbowllast.png';
-import bgMb from '../../public/image/foodbowlmoblie.png';
+
 
 
 export default function Home() {
   const [section, setSection] = React.useState(0)
+
   return (
     <NextUIProvider>
       <main className="flex min-h-screen flex-col items-center justify-between relative w-full h-full">
-      {/* {section === 0 && (
-          <>
-          <Image src={bg} alt="background-image" fill className="-z-5 object-bottom object-contain hidden sm:block"/>
-          <Image src={bgMb} alt="background-image" fill className="-z-5 object-bottom object-contain sm:hidden" />
-          </>
-
-        )}  */}
         <div className="w-full h-full">
           <HomePage section={section} setSection={setSection} /> 
         </div>
-        {/* Background image */}
-        {/* <div className="w-full h-full absolute top-0 left-0">
-          <Image src={bg} alt="background-image" fill className="object-bottom object-contain sm:hidden" />
-        </div> */}
       </main>
     </NextUIProvider>
   );
